58ffe4852ca62502c8bd6379ed4b365a416e8579
cassaram09 <cassaram09@gmail.com>
2017-07-04 19:48:29 -0400

    break up template file

--- template.js
+++ template.js
@@ -1,7 +1,4 @@
-exports.main = `
-import React, {Component} from 'react'
-import PropTypes from 'prop-types'
-
+const main = `
 class :className extends Component {
   constructor(props){
     super(props)
@@ -19,6 +16,38 @@ class :className extends Component {
 :className.propTypes = {
 
 }
-
-export default :className;
 `
+
+const imports = {
+  react: "import React, {Component} from 'react'",
+  propTypes: "import PropTypes from 'prop-types'",
+  bindActionCreators: "import {bindActionCreators} from 'redux'",
+  connect: "import {connect} from 'react-redux'"
+}
+
+const exported = {
+  default: "export default :className;"
+  connectState: "export default connect(mapStateToProps)(:className);"
+  connectDispatch: "export default connect(null, mapDispatchToProps)(:className);"
+  connectStateAndDispatch: "export default connect(mapStateToProps, mapDispatchToProps)(:className);"
+}
+
+const mapStateToProps = `
+function mapStateToProps(state, ownProps) { 
+  return {};
+};
+`
+
+const mapDispatchToProps = `
+function mapDispatchToProps(dispatch) {  
+  return {};
+}
+`
+
+module.exports = {
+  main: main,
+  imports: imports,
+  exports: exported,
+  state: mapStateToProps,
+  dispatch: mapDispatchToProps
+}
