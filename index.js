7d4eb70a98db9cb87b8e24c48e84da2e246cae8b
cassaram09 <cassaram09@gmail.com>
2017-07-04 20:45:33 -0400

    add comments, refactor code, add IIFE

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
 #!/usr/bin/env node --harmony
 
-const fs = require('fs')
-const program = require('commander'); 
-const chalk = require('chalk'); 
-const replace = require('replace');
-const template = require('./template');
+// Don't pollute our global scope!
+(function () {
+  const fs = require('fs')
+  const program = require('commander'); 
+  const chalk = require('chalk'); 
+  const replace = require('replace');
+  const template = require('./template');
 
-program
- .arguments('<className>')
- .option('-p, --path <path>', 'Add path to new file (relative from current directory)')
- .option('-d, --dispatch', 'Add dispatch')
- .option('-s, --state', 'Add state')
- .action(createComponent)
- .parse(process.argv);
+  program
+   .arguments('<className>')
+   .option('-p, --path <path>', 'Add path to new file (relative from current directory)')
+   .option('-d, --dispatch', 'Add dispatch')
+   .option('-s, --state', 'Add state')
+   .action(createComponent)
+   .parse(process.argv);
 
-function writeFile(className){
-  var path;
-  if (program.path){
-    path = program.path + '/' + className
-  } else {
-    path = className;
-  }
+  // Write the template to a file and replace our className variables
+  function writeFile(template, className){
+    var path;
+    program.path ? path = (program.path + '/' + className) : path = className;
 
-  var template = buildTemplate();
-
-  fs.writeFile(`${path}.js`, template, (err) => {
+    fs.writeFile(`${path}.js`, template, (err) => {
       if (err) throw err;
       replace({
         regex: ":className",
@@ -35,66 +32,84 @@ function writeFile(className){
       });
       console.log(chalk.bold.cyan(`Component ${className} created at ${path}.js`))
     });
-}
-
-function checkPath(path) {
-  if ( path !== undefined ){
-    return fs.existsSync(path)
   }
-}
 
-function capitalize(className){
-  return className[0].toUpperCase() + className.substring(1, className.length)
-}
-
-function createComponent(className){
-  if ( program.path && !checkPath(program.path) ) {
-    fs.mkdir(program.path, function (err) {
-      if (err) {
-        console.log('failed to create directory', err);
-      }
-    })
+  // check if the --path was passed or exists
+  function validatePath(path) {
+    return path !== undefined ? fs.existsSync(path) : false;
   }
-  writeFile(capitalize(className))
-}
 
-function buildTemplate(){
-  return buildImports() + '\n' + buildBody() + '\n' + buildExports()
-}
+  // create a directory if passed a --path option and no directory exists
+  function createDirectory(){
+    if ( !validatePath(program.path) ) {
+      fs.mkdir(program.path, function (err) {
+        if (err) throw err;
+      })
+    }
+  }
 
-function buildImports(){
-  var imports = [template.imports.react, template.imports.propTypes]
-  if ( program.state || program.dispatch ) {
-    imports.push(template.imports.connect)
+  // capitalize our className in case the user forgets (only first letter...)
+  function capitalize(className){
+    return className[0].toUpperCase() + className.substring(1, className.length)
   }
-  if ( program.dispatch ) {
-    imports.push(template.imports.bindActionCreators)
-  }
-  return imports.join('\n')
-}
 
-function buildBody(){
-  var body = [template.main]
-  if ( program.state) {
-    body.push(template.state)
+  // build our new component
+  function createComponent(className){
+    var template = buildTemplate()
+    var name = capitalize(className)
+    createDirectory();
+    writeFile(template, name)
   }
-  if ( program.dispatch) {
-    body.push(template.dispatch)
-  }
-  return body.join('\n')
-}
 
-function buildExports(){
-  var exported = []
-  if ( program.state && program.dispatch ) {
-    exported.push(template.exported.connectStateAndDispatch);
+  // build the template - we'll replace variables later
+  function buildTemplate(){
+    return buildImports() + '\n' + buildBody() + '\n' + buildExports()
+  }
+
+  // build the imports for our component
+  function buildImports(){
+    var imports = [template.imports.react, template.imports.propTypes]
+    if ( program.state || program.dispatch ) {
+      imports.push(template.imports.connect)
+    }
+    if ( program.dispatch ) {
+      imports.push(template.imports.bindActionCreators)
+    }
+    return imports.join('\n')
+  }
+
+  // build the body of our component
+  function buildBody(){
+    var body = [template.main]
+    if ( program.state) {
+      body.push(template.state)
+    }
+    if ( program.dispatch) {
+      body.push(template.dispatch)
+    }
+    return body.join('\n')
+  }
+
+  // build the exports for our component
+  function buildExports(){
+    var exported = []
+    if ( !program.state && !program.dispatch ) {
+      exported.push(template.exported.default);
+      return exported.join('')
+    }
+    if ( program.state && program.dispatch ) {
+      exported.push(template.exported.connectStateAndDispatch);
+      return exported.join('')
+    }
+    if ( program.state ) {
+      exported.push(template.exported.connectState);
+    }
+    if ( program.dispatch ) {
+      exported.push(template.exported.connectDispatch);
+    }
+
     return exported.join('')
   }
-  if ( program.state ) {
-    exported.push(template.exported.connectState);
-  }
-  if ( program.dispatch ) {
-    exported.push(template.exported.connectDispatch);
-  }
-  return exported.join('')
-}
+
+ }
+)()
